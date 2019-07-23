84aaf3e5df4514ab40f7683fcc20c82f5265af31
DomiasPL <dominik-stepuch@wp.pl>
2019-07-23 14:47:12 +0200

    @Disabled and @DisplayName annotations

--- src/test/java/pl/github/DominasPL/JUnit5/CartTest.java
+++ src/test/java/pl/github/DominasPL/JUnit5/CartTest.java
@@ -1,15 +1,20 @@
 package pl.github.DominasPL.JUnit5;
 
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Test cases for cart")
 class CartTest {
 
     //testy wydajnościowe
+//    @Disabled
     @Test
+    @DisplayName("Cart is able to process 1000 orders in 5ms")
     void simulateLargeOrder() {
 
         //given
@@ -17,7 +22,7 @@ class CartTest {
 
         //when
         //then
-        assertTimeout(Duration.ofMillis(1), () -> cart.simulateLargeOrder());
+        assertTimeout(Duration.ofMillis(5), () -> cart.simulateLargeOrder());
 
 
     }
