69ce4b296265d73ca1b3343b62e2e703aeb194b0
DomiasPL <dominik-stepuch@wp.pl>
2019-07-24 14:23:42 +0200

    Dynamic test, @TestFactory annotation

--- src/test/java/pl/github/DominasPL/JUnit5/MealTest.java
+++ src/test/java/pl/github/DominasPL/JUnit5/MealTest.java
@@ -1,6 +1,8 @@
 package pl.github.DominasPL.JUnit5;
 
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -8,7 +10,9 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -128,6 +132,35 @@ class MealTest {
         }
 
         assertThat(price, lessThan(20));
+    }
+
+    @TestFactory
+    Collection<DynamicTest> calculateMealPrices() {
+        Order order = new Order();
+        order.addMealToOrder(new Meal(10, 2, "Hamburger"));
+        order.addMealToOrder(new Meal(5, 3, "Pizza"));
+        order.addMealToOrder(new Meal(1, 10, "Apple"));
+
+        Collection<DynamicTest> dynamicTests = new ArrayList<>();
+
+        for (int i = 0; i < order.getMeals().size(); i++) {
+            int price = order.getMeals().get(i).getPrice();
+            int quantity = order.getMeals().get(i).getQuantity();
+
+            Executable executable = () -> {
+                assertThat(calculatePrice(price, quantity), lessThan(30));
+            };
+
+            String name = "Test name " + i;
+            DynamicTest dynamicTest = DynamicTest.dynamicTest(name, executable);
+            dynamicTests.add(dynamicTest);
+        }
+
+        return dynamicTests;
+    }
+
+    private int calculatePrice(int price, int quantity) {
+        return price*quantity;
     }
 
 }
