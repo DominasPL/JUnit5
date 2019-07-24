a255b17085f2a0316c3aa3d9b24b7d0c55eebdda
DomiasPL <dominik-stepuch@wp.pl>
2019-07-24 12:37:21 +0200

    @CsvFileSource annotation

--- src/test/java/pl/github/DominasPL/JUnit5/AddressTest.java
+++ src/test/java/pl/github/DominasPL/JUnit5/AddressTest.java
@@ -1,6 +1,7 @@
 package pl.github.DominasPL.JUnit5;
 
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,6 +13,16 @@ class AddressTest {
     @ParameterizedTest
     @CsvSource({"Fabryczna, 10", "Armii Krajowej, 15c", "Antoniego Batorego, 40d"})
     void givenAddressesShouldNotBeEmptyAndHaveProperNames(String street, String number) {
+        assertThat(street, notNullValue());
+        assertThat(street, containsString("a"));
+        assertThat(number, notNullValue());
+        assertThat(number.length(), lessThan(8));
+    }
+
+    //Wartosci z pliku csv
+    @ParameterizedTest
+    @CsvFileSource(resources = "/addresses.csv")
+    void addressesFromFileShouldNotBeEmptyAndHaveProperNames(String street, String number) {
         assertThat(street, notNullValue());
         assertThat(street, containsString("a"));
         assertThat(number, notNullValue());
